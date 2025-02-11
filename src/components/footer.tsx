c1b0fe974702886635128a09ce1c60d83bf804b5
santoshallu1234 <santoshallu1234@gmail.com>
2025-02-11 20:19:30 +0530

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,32 +4,32 @@ import img from "../../Speakify.png"
 export default function Footer() {
   return (
     <footer className="bg-black text-white py-8">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+      <div className="container mx-auto flex flex-col px-10 md:flex-row justify-between items-center">
         <div className="mb-4 md:mb-0">
           <Image src={img} alt="Translation Logo" width={50} height={50} />
         </div>
         <nav>
           <ul className="flex space-x-4">
             <li>
-              <Link href="/about" className="hover:text-[#5e17eb]">
+              <Link href="/about" className="hover:text-[#5e17eb] yo">
                 About
               </Link>
             </li>
             <li>
-              <Link href="/contact" className="hover:text-[#5e17eb]">
+              <Link href="/contact" className="hover:text-[#5e17eb] yo">
                 Contact
               </Link>
             </li>
             <li>
-              <Link href="/privacy" className="hover:text-[#5e17eb]">
+              <Link href="/privacy" className="hover:text-[#5e17eb] yo">
                 Privacy Policy
               </Link>
             </li>
           </ul>
         </nav>
         <div className="mt-4 md:mt-0">
-          <Link href="https://twitter.com/yourusername" className="text-[#5e17eb] hover:underline">
-            @yourusername
+          <Link href="https://twitter.com/yourusername" className="text-[#5e17eb] yo hover:underline">
+            @santoshallu1234@gmail.com
           </Link>
         </div>
       </div>
